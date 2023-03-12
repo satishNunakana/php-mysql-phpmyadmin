e438b38f513b7dd558fe930eed4b2b328973a076
satishNunakana <76905574+satishNunakana@users.noreply.github.com>
2023-03-12 17:40:15 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'moe-mysql-app');
-define('DB_USERNAME', 'moeuser');
-define('DB_PASSWORD', 'moepass');
-define('DB_NAME', 'moe_db');
+define('DB_SERVER', 'mysql-app');
+define('DB_USERNAME', 'satish');
+define('DB_PASSWORD', 'satish');
+define('DB_NAME', 'demo_db');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
